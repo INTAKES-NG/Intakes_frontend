22aae929d62f95119abfeaa11ea8b3191b1287db
Sewo Goodness <sewogoodness111@gmail.com>
2022-12-07 01:26:10 +0100

    finished did you know section

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
   Contact,
   Error,
 } from "./components";
+import DidYouKnowSingle from "./pages/DidYouKnowSingle";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/restaurant" element={<Restaurant />} />
         <Route path="/didYouKnow" element={<DidYouKnow />} />
+        <Route path="/didYouKnow/:id" element={<DidYouKnowSingle />} />
         <Route path="/news" element={<News />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
