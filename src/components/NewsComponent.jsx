16962a76908ae1809f225669e2b8191746b6ded3
William <anazawilliam1@gmail.com>
2023-01-19 19:34:41 +0100

    news section updated

--- src/components/NewsComponent.jsx
+++ src/components/NewsComponent.jsx
@@ -5,7 +5,6 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import newsIcon from "../assets/news-icon.png";
 import greenIcon from "../assets/green-nav-arrow.png";
-import WhiteIcon from "../assets/white-nav-arrow.png";
 
 // loader
 import { FallingLines } from "react-loader-spinner";
@@ -90,173 +89,97 @@ const NewsComponent = () => {
       </div>
       <div className="news-container-wrapper">
         <div className="news-container">
-          {/* news items */}
           {/* first news item */}
-          <div className="news-item news-item-col-1">
+          <div className="news-item">
             <div className="news-row">
               <div
                 style={{
                   backgroundImage: `url(${newsData[0]?.image_url})`,
-                  height: "298px",
-                  width: "320px",
+                  height: "230px",
+                  width: "100%",
                   backgroundPosition: "center",
                   backgroundSize: "cover",
                   flexBasis: "40%",
                 }}
+                className="image__row"
               ></div>
               <div className="text-col">
+                <div className="news-date">{newsData[0].pubDate}</div>
                 <Link to={`/new/${newsData[0]?.title}`} state={newsData[0]}>
                   <h3 className="news-title">{newsData[0]?.title}</h3>
                 </Link>
 
-                <p className="news-description">{newsData[0]?.description}</p>
-                <div className="inner-row">
-                  <div className="news-date">{newsData[0].pubDate}</div>
-                  <div className="news-link">
-                    <Link
-                      to={`/new/${newsData[0]?.title}`}
-                      state={newsData[0]}
-                    >
-                      <img
-                        src={greenIcon}
-                        alt=""
-                        style={{
-                          width: "30px",
-                          height: "30px",
-                          cursor: "pointer",
-                        }}
-                      />
-                    </Link>
-                  </div>
+                {/* <p className="news-description">{newsData[0]?.description}</p> */}
+
+                <div className="news-link">
+                  <Link to={`/new/${newsData[0]?.title}`} state={newsData[0]}>
+                    <p>Read</p>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
           {/* second news item */}
-          <div className="news-item news-item-col-2">
-            <div className="row">
+          <div className="news-item">
+            <div className="news-row">
               <div
                 style={{
                   backgroundImage: `url(${newsData[1]?.image_url})`,
-                  height: "120px",
+                  height: "230px",
                   width: "100%",
                   backgroundPosition: "center",
                   backgroundSize: "cover",
+                  flexBasis: "40%",
                 }}
+                className="image__row"
               ></div>
-              <div className="col-2">
+              <div className="text-col">
+                <div className="news-date">{newsData[1].pubDate}</div>
                 <Link to={`/new/${newsData[1]?.title}`} state={newsData[1]}>
                   <h3 className="news-title">{newsData[1]?.title}</h3>
                 </Link>
 
-                <p className="news-description">{newsData[1]?.description}</p>
-                <div className="inner-row">
-                  <div className="news-date">{newsData[1].pubDate}</div>
-                  <div className="news-link">
-                    <Link
-                      to={`/new/${newsData[1]?.title}`}
-                      state={newsData[1]}
-                    >
-                      <img
-                        src={WhiteIcon}
-                        alt=""
-                        style={{
-                          width: "30px",
-                          height: "30px",
-                          cursor: "pointer",
-                        }}
-                      />
-                    </Link>
-                  </div>
+                {/* <p className="news-description">{newsData[1]?.description}</p> */}
+
+                <div className="news-link">
+                  <Link to={`/new/${newsData[1]?.title}`} state={newsData[1]}>
+                    <p>Read</p>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        {/* third */}
-        <div className="news-container news-container-2">
-          {/* third news item */}
-          <div className="news-item news-item-col-1">
+          {/* third */}
+          <div className="news-item">
             <div className="news-row">
               <div
                 style={{
                   backgroundImage: `url(${newsData[2]?.image_url})`,
-                  height: "299px",
-                  width: "320px",
+                  height: "230px",
+                  width: "100%",
                   backgroundPosition: "center",
                   backgroundSize: "cover",
                   flexBasis: "40%",
                 }}
               ></div>
               <div className="text-col">
+                <div className="news-date">{newsData[2].pubDate}</div>
                 <Link to={`/new/${newsData[2]?.title}`} state={newsData[2]}>
                   <h3 className="news-title">{newsData[2]?.title}</h3>
                 </Link>
 
-                <p className="news-description">{newsData[2]?.description}</p>
-                <div className="inner-row">
-                  <div className="news-date">{newsData[2].pubDate}</div>
-                  <div className="news-link">
-                    <Link
-                      to={`/new/${newsData[2]?.title}`}
-                      state={newsData[2]}
-                    >
-                      <img
-                        src={greenIcon}
-                        alt=""
-                        style={{
-                          width: "30px",
-                          height: "30px",
-                          cursor: "pointer",
-                        }}
-                      />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* fourth news item */}
-          <div className="news-item news-item-col-2">
-            <div className="row">
-              <div
-                style={{
-                  backgroundImage: `url(${newsData[4]?.image_url})`,
-                  height: "120px",
-                  width: "100%",
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                }}
-              ></div>
-              <div className="col-2">
-                <Link to={`/new/${newsData[4]?.title}`} state={newsData[4]}>
-                  <h3 className="news-title">{newsData[4]?.title}</h3>
-                </Link>
+                {/* <p className="news-description">{newsData[2]?.description}</p> */}
 
-                <p className="news-description">{newsData[4]?.description}</p>
-                <div className="inner-row">
-                  <div className="news-date">{newsData[4].pubDate}</div>
-                  <div className="news-link">
-                    <Link
-                      to={`/new/${newsData[4]?.title}`}
-                      state={newsData[4]}
-                    >
-                      <img
-                        src={WhiteIcon}
-                        alt=""
-                        style={{
-                          width: "30px",
-                          height: "30px",
-                          cursor: "pointer",
-                        }}
-                      />
-                    </Link>
-                  </div>
+                <div className="news-link">
+                  <Link to={`/new/${newsData[2]?.title}`} state={newsData[2]}>
+                    <p>Read</p>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        {/*  */}
       </div>
     </div>
   );
